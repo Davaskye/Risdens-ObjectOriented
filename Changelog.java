3d01b85f5e25a850297081a57726656317f3c73d
AllahDalla <alromariodavis@gmail.com>
2022-03-14 18:53:00 -0500

    Updated changelog with extended constructor and getters

--- Changelog.java
+++ Changelog.java
@@ -3,38 +3,52 @@ package proj;
 public class Changelog {
     private String date;
     private String time;
-    private String change;
-    private String name;
+    private String productname;
+    private String quantity;
+    private String cost;
 
-    public Changelog(String date, String time, String personName, String change)
-    {
-        
+    public Changelog(String date, String time, String productname, String quantity, String cost) {
+
         this.date = date;
         this.time = time;
-        this.name = personName;
-        this.change = change;
+        this.productname = productname;
+        this.quantity = quantity;
+        this.cost = cost;
     }
 
-
-    public String getDate()
-    {
+    public String getDate() {
         return date;
     }
 
-    public String getTime()
-    {
+    public String getTime() {
         return time;
     }
 
-
-    public String getChange()
-    {
-        return change;
+    public String getProductName() {
+        return productname;
     }
 
-    public String getName()
-    {
-        return name;
+    public String getQuantity() {
+        return quantity;
     }
-    
+
+    public String getCost() {
+        return cost;
+    }
+
+    // public String getChange()
+    // {
+    // return change;
+    // }
+
+    // public String getName()
+    // {
+    // return name;
+    // }
+
+    public static void main(String[] args) {
+        Changelog test = new Changelog("date", "time", "bleach", "10", "150");
+        System.out.println(test.getProductName());
+    }
+
 }

3668dd8a4129f72773f4ca278ee2dae9cbae503a
Fre99 <75258736+Fre99@users.noreply.github.com>
2022-04-03 14:55:22 -0500

    Update

---
+++ OrderManager.java
@@ -0,0 +1,61 @@
+ 
+public final class OrderManager {
+
+    public Order order;
+    public int itemId;
+
+    public OrderManager() {}
+
+    public OrderManager(Order order, int itemId) {
+        this.setOrder(order);
+        this.setItemId(itemId);
+    } 
+
+    private Object getOrderId() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "" + getOrder() + "-" + getItemId();
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return ((this.getOrder() == null
+                ? 0 : this.getOrder().hashCode())
+                ^ ((int) this.getItemId()));
+    }
+
+    @Override
+    public boolean equals(Object otherOb) {
+        if (this == otherOb) {
+            return true;
+        }
+        if (!(otherOb instanceof OrderManager)) {
+            return false;
+        }
+        OrderManager other = (OrderManager) otherOb;
+        return ((this.getOrder() == null
+                ? other.getOrder() == null : this.getOrderId()
+                .equals(other.getOrder())) 
+                && (this.getItemId() == other.getItemId()));
+}
+}
